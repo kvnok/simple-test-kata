9a7dedbddfc947e397ed6187513d86ff7aacfa91
Kevin Kroon <kevinjankroon@gmail.com>
2024-03-16 23:21:47 +0100

    other solution from random git

--- cool.js
+++ cool.js
@@ -1,39 +1,59 @@
-function solve(str) {
-	console.log(str);
-    let result = '';
-    let signStack = [];
-    let currentSign = 1;
-
-    const operations = {
-        '+': () => { currentSign = 1; },
-        '-': () => { currentSign = -1; },
-        '(': () => { signStack.push(currentSign); },
-        ')': () => { signStack.pop(); },
-        default: (char) => {
-            if (signStack.length > 0) {
-                result += (currentSign * signStack[signStack.length - 1] === -1) ? '-' : '+';
-            } else {
-                result += (currentSign === -1) ? '-' : '+';
-            }
-            result += char;
-        }
-    };
-
-    for (let i = 0; i < str.length; i++) {
-        const char = str[i];
-        const operation = operations[char] || operations.default;
-        operation(char);
+function solve(s) {
+    let stack = []
+    let sign = 1
+    let res = ''
+  
+    for (let c of s) {
+      switch (c) {
+        case '(':
+          stack.push(sign)
+          sign = 1
+          break
+        case ')':
+          stack.pop()
+          sign = 1
+          break
+        case '-':
+          sign = -1
+          break
+        case '+':
+          sign = 1
+          break
+        default:
+          res += (stack.reduce((acc, v) => acc * v, 1) * sign === 1 ? '+' : '-') + c
+      }
     }
-	if (result[0] === '+') {
-		result = result.slice(1);
-	}
-	console.log(result);
-    return result;
+    return res.replace(/^\+/, '');
 }
+
+// console.log(solve("a-(b)"));
+// console.log(solve("a-(-b)"));
+// console.log(solve("a+(b)"));
+// console.log(solve("a+(-b)"));
+// console.log(solve("(((((((((-((-(((n))))))))))))))"));
+// console.log(solve("(((a-((((-(-(f)))))))))"));
+// console.log(solve("((((-(-(-(-(m-g))))))))"));
+// console.log(solve("(((((((m-(-(((((t)))))))))))))"));
+// console.log(solve("-x"));
+// console.log(solve("-(-(x))"));
+// console.log(solve("-((-x))"));
+// console.log(solve("-(-(-x))"));
+// console.log(solve("-(-(x-y))"));
+// console.log(solve("-(x-y)"));
+// console.log(solve("x-(y+z)"));
+// console.log(solve("x-(y-z)"));
+// console.log(solve("x-(-y-z)"));
+// console.log(solve("x-(-((-((((-((-(-(-y)))))))))))"));
+// console.log(solve("u-(v-w+(x+y))-z"));
+// console.log(solve("x-(s-(y-z))-(a+b)"));
+// console.log(solve("u+(g+v)+(r+t)"));
+// console.log(solve("q+(s-(x-o))-(t-(w-a))"));
+// console.log(solve("u-(v-w-(x+y))-z"));
+// console.log(solve("v-(l+s)-(t+y)-(c+f)+(b-(n-p))"));
 
 module.exports = solve;
 
-//https://www.codewars.com/kata/5a3bedd38f27f246c200005f/train/javascript
+//https://www.codewars.com/kata/simple-parenthesis-removal
 // In this Kata, you will be given a mathematical string and your task will be to remove all braces as follows:
 
 // solve("x-(y+z)") = "x-y-z"
